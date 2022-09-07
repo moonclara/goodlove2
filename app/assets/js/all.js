81c62481b1225216519776d99cd0c3daa79bb990
Clara <smile629639@gmail.com>
2022-09-07 10:37:26 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,25 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+  //購物說明
+  $('.shop-direction .collapse').on('shown.bs.collapse', function () {
+    $(this).parent().find(".fa-chevron-down").removeClass("fa-chevron-down").addClass("fa-chevron-up");
+  }).on('hidden.bs.collapse', function () {
+    $(this).parent().find(".fa-chevron-up").removeClass("fa-chevron-up").addClass("fa-chevron-down");
+  });
+
+  //aos
+  AOS.init();
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
